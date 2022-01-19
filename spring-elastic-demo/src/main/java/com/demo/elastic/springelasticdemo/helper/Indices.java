31f5fed6c78c4938cd899e32f1d1a6af0bf50b0f
masonxfan <mason.xfan@gmail.com>
2022-01-19 13:53:34 +1100

    20220119elasticsearch learn

--- spring-elastic-demo/src/main/java/com/demo/elastic/springelasticdemo/helper/Indices.java
+++ spring-elastic-demo/src/main/java/com/demo/elastic/springelasticdemo/helper/Indices.java
@@ -3,4 +3,6 @@ package com.demo.elastic.springelasticdemo.helper;
 public class Indices {
 
 	public static final String PERSON_INDEX = "person";
+	
+	public static final String VEHICLE_INDEX = "vehicle";
 }
